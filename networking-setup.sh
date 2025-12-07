efa723bd44255e335711e91c28c403ccec8430e9
vmpublic <1993pluto1@protonmail.com>
2025-12-07 18:53:37 +1300

    Update networking-setup.sh

--- networking-setup.sh
+++ networking-setup.sh
@@ -4,7 +4,5 @@ set -eux
 ip addr add 10.0.0.2/24 dev ens5
 ip link set ens5 up
 ip route add default via 10.0.0.1
-printf 'nameserver 1.1.1.1\n' >> /etc/resolv.conf
-printf 'nameserver 1.0.0.1\n' >> /etc/resolv.conf
 printf 'Initial setup complete. Now pinging cloudflare.com...\n'
 ping -c 4 cloudflare.com
