58ccde5467d4453f8bb03c85c16f05daaa6ac05f
vmpublic <1993pluto1@protonmail.com>
2025-12-25 00:22:40 +1300

    Update networking-auto-setup.sh

--- networking-auto-setup.sh
+++ networking-auto-setup.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -eux
-# assuming primary interface is ens5
-ip addr add 10.0.0.2/24 dev ens5
-ip link set ens5 up
+# assuming primary interface is eth0
+ip addr add 10.0.0.2/24 dev eth0
+ip link set eth0 up
 ip route add default via 10.0.0.1
 printf 'Initial setup complete. Now pinging cloudflare.com...\n'
 ping -c 4 cloudflare.com
