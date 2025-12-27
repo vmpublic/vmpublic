649a3bdb8c44fdeac9b87fb3533207188182048a
vmpublic <1993pluto1@protonmail.com>
2025-12-27 17:38:01 +1300

    Create 0-0-0-isolated-github-auth.sh

---
+++ 0-0-0-isolated-github-auth.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# -----------------------------
+# Prompt for GitHub username and Personal Access Token (PAT)
+# -----------------------------
+printf 'Enter GitHub username: '
+read -r GITHUB_USERNAME
+export GITHUB_USERNAME
+printf 'Enter GitHub Personal Access Token (PAT): '
+read -r GITHUB_TOKEN
+export GITHUB_TOKEN
+# -----------------------------
+# Github authentication
+# -----------------------------
+mkdir -p /root/.ssh
+# # # # # Generate SSH key
+ssh-keygen -t rsa -b 4096 -f /root/.ssh/id_rsa -N ""
+# Upload public key to github
+set +x
+curl -H "Authorization: token ${GITHUB_TOKEN}" -H "Content-Type: application/json" -d "$(jq -n --arg title "Alpine Linux $(hostname)" --arg key "$(cat /root/.ssh/id_rsa.pub)" '{title:$title, key:$key}')" https://api.github.com/user/keys
+set -x
+# # # # # Configure SSH config
+tee /root/.ssh/config <<'EOC'
+Host github.com
+User git
+IdentityFile /root/.ssh/id_rsa
+IdentitiesOnly yes
+EOC
+# # # # # Preseed known_hosts file
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+# # # # # Configure chmod permissions
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_rsa
+chmod 600 /root/.ssh/id_rsa.pub
+chmod 600 /root/.ssh/config
+chmod 600 /root/.ssh/known_hosts
+# # # # # Connect to Github (including || true otherwise exits even on success)
+ssh -T git@github.com || true
+unset GITHUB_TOKEN
+unset GITHUB_USERNAME
