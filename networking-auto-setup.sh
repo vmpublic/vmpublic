9b4ad3d3f4c102c6be6572b87249b2d0b5769645
vmpublic <vmpublic@users.noreply.github.com>
2026-02-11 20:28:01 +1300

    update

--- networking-auto-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-set -eux
-# assuming primary interface is eth0
-ip addr add 10.0.0.2/24 dev eth0
-ip link set eth0 up
-ip route add default via 10.0.0.1
-printf 'Initial setup complete. Now pinging cloudflare.com...\n'
-ping -c 4 cloudflare.com
