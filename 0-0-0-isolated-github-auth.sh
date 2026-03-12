ee14d377c8fdc22c361fdfdc0de5762bbfac325f
vmpublic <1993pluto1@protonmail.com>
2026-03-13 08:56:01 +1300

    Create 0-0-0-isolated-github-auth.sh

---
+++ 0-0-0-isolated-github-auth.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# -----------------------------
+# Prompt for GitHub username and Personal Access Token (PAT)
+# -----------------------------
+printf 'Enter GitHub Personal Access Token (PAT): '
+read -r GITHUB_TOKEN
+export GITHUB_TOKEN
+
+# -----------------------------
+# apk add dependencies
+# -----------------------------
+apk add git curl jq openssh
+
+# -----------------------------
+# Github authentication
+# -----------------------------
+mkdir -p /root/.ssh
+
+# # # # # Generate Ed25519 SSH key (Replaces RSA)
+ssh-keygen -t ed25519 -a 100 -f /root/.ssh/id_ed25519 -N ""
+
+# Upload public key to github
+set +x
+curl -H "Authorization: token ${GITHUB_TOKEN}" \
+     -H "Content-Type: application/json" \
+     -d "$(jq -n --arg title "Alpine $(hostname) Ed25519" --arg key "$(cat /root/.ssh/id_ed25519.pub)" '{title:$title, key:$key}')" \
+     https://api.github.com/user/keys
+set -x
+
+# # # # # Configure SSH config (Updated for Ed25519)
+tee /root/.ssh/config <<'EOC'
+Host github.com
+  User git
+  IdentityFile /root/.ssh/id_ed25519
+  IdentitiesOnly yes
+EOC
+
+# # # # # Preseed known_hosts file
+ssh-keyscan github.com >> /root/.ssh/known_hosts
+
+# # # # # Configure chmod permissions
+chmod 700 /root/.ssh
+chmod 600 /root/.ssh/id_ed25519
+chmod 644 /root/.ssh/id_ed25519.pub
+chmod 600 /root/.ssh/config
+chmod 600 /root/.ssh/known_hosts
+
+# # # # # Connect to Github
+ssh -T git@github.com || true
+
+unset GITHUB_TOKEN
